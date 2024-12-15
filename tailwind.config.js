3f814d9e56bf5470b3b185c9545165c40cc9f0ea
DPolar05 <danaaro9805@gmail.com>
2024-12-15 00:35:59 -0600

    Crear carpetas

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,html}"],
+	theme: {
+		fontFamily: {
+			'man':['Manjari'],'ques':['Questrial'],'did':['Didact Gothic'],
+		},
+		
+		extend: {
+			colors: {'azul-oscuro':'#001e2b'}
+		},
+		},
+		plugins:[],
+	}
